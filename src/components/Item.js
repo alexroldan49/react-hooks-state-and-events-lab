2868e52d931f5e753db88160d0dd91a4ab0fdee7
Alexander Roldan <alexroldan49@gmail.com>
2021-08-12 19:42:56 -0400

    commit tree 4116f7e83cfe4781dd9378f37756e74623fa505e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react"
 
 function Item({ name, category }) {
+
+  const [cName, setCName] = useState(false)
+  
+function clickHandler (){
+  setCName(!cName)
+}
+
   return (
-    <li className="">
+    <li className={ cName ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={()=>{clickHandler()}} className={ cName ? "remove" : "add" }>{ cName ? "Remove From Cart" : "Add To Cart" }</button>
     </li>
   );
 }
